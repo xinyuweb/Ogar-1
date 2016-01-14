01705bb0629f9f1d660930ed0dc080963140bac4
Castia994 <ItzLevvie@users.noreply.github.com>
2016-01-14 18:21:50 +0000

    Update Virus.js

--- src/entity/Virus.js
+++ src/entity/Virus.js
@@ -9,31 +9,20 @@ module.exports = Virus, Virus.prototype = new Cell, Virus.prototype.calcMove = n
 }, Virus.prototype.getEatingRange = function ()
 {
   return .4 * this.getSize()
-}, Virus.prototype.onAutoMove = function (e)
-{
-  for (var s = 100, i = e.nodesEjected.length, o = 0; i > o; o++)
-  {
-    var t = e.nodesEjected[o],
-      n = t.position.y - s,
-      r = t.position.y + s,
-      a = t.position.x - s,
-      l = t.position.x + s;
-    this.collisionCheck(r, n, l, a) && (t.angle = this.angle, this.feed(t, e), o--, i--)
-  }
 }, Virus.prototype.onConsume = function (e, s)
 {
   var i = e.owner,
-    o = Math.floor(e.mass / 16) - 1,
-    t = s.config.playerMaxCells - i.cells.length;
-  t = Math.min(t, o);
-  var n = Math.min(e.mass / (t + 1), 36);
-  if (e.addMass(this.mass), !(0 >= t))
+    t = Math.floor(e.mass / 16) - 1,
+    o = s.config.playerMaxCells - i.cells.length;
+  o = Math.min(o, t);
+  var r = Math.min(e.mass / (o + 1), 36);
+  if (e.addMass(this.mass), !(0 >= o))
   {
-    var r = 0,
-      a = e.mass - t * n;
-    a > 300 && t > 0 && (r++, t--), a > 1200 && t > 0 && (r++, t--), a > 3e3 && t > 0 && (r++, t--);
-    for (var l = 0, u = 0; t > u; u++) l += 6 / t, s.newCellVirused(i, e, l, n, 150), e.mass -= n;
-    for (var u = 0; r > u; u++) l = 6.28 * Math.random(), n = e.mass / 4, s.newCellVirused(i, e, l, n, 20), e.mass -= n;
+    var n = 0,
+      a = e.mass - o * r;
+    a > 300 && o > 0 && (n++, o--), a > 1200 && o > 0 && (n++, o--), a > 3e3 && o > 0 && (n++, o--);
+    for (var l = 0, u = 0; o > u; u++) l += 6 / o, s.newCellVirused(i, e, l, r, 150), e.mass -= r;
+    for (var u = 0; n > u; u++) l = 6.28 * Math.random(), r = e.mass / 4, s.newCellVirused(i, e, l, r, 20), e.mass -= r;
     e.calcMergeTime(s.config.playerRecombineTime)
   }
 }, Virus.prototype.onAdd = function (e)
